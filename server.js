97f4a56a43b5d28f2b401a82cf0493ad144dd0f7
ragingpenguin <eflinjh@gmail.com>
2023-02-10 14:15:52 -0500

    honk

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = require('./routes')
 const fs = require('fs/promises');
 
+const PORT= 3001;
+
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(express.static('public'));
 
+app.listen (PORT, () => {
+    console.log("App listening at http://localhost:${PORT} 🚀")
+})
