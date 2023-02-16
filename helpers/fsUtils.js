73f4529f5a69ca59fe95d9d821b3fb1487747e5b
ragingpenguin <eflinjh@gmail.com>
2023-02-16 15:04:13 -0500

    made the helper file promise based instead of using fs and utils.removed test note

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,26 +1,32 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require('fs/promises');
+
 
 // Promise version of fs.readFile
 
-const readFromFile = util.promisify(fs.readFile);
+const readFromFile = fs.readFile;
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
+const writeToFile = (destination, content) => {
+  fs.writeFile(destination, JSON.stringify(content, null, 4))
+  .then((data) => {
+    console.info(`\nData written to ${destination}`)
+})
+  .catch((err) => {
+    console.error(err)
+  })
+}
+
 
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    console.log(data)
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
+  fs.readFile(file, 'utf8')
+   .then((data)=> {
+   const parsedData = JSON.parse(data);
       parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
+      writeToFile(file, parsedData); 
+    console.log(data)
+   })
+    .catch((err) =>{
+      console.error(err);  
+  })
+}
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
